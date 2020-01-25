101f7f655f2e7e5754defae2d98d1564d13c468f
Plex Luthor <plexluthor@yahoo.com>
2020-01-25 13:19:34 -0500

    Initial check-in of the caching code for matrix inversion.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+# Following the example in the assignment, we create a 4-element
+# vector of getter/setter functions for the data matrix and its
+# inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
 ## Write a short comment describing this function
-
+# Following the example in the assignment, we check if the cached
+# value is null. If not, return the cached value. Otherwise,
+# compute the inverse, cache it, and return it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
